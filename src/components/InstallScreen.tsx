23d60c7010a5ece549c90e7fc8bb5130b92c3766
renatosilva2020 <professorrsilva2020@gmail.com>
2026-04-05 22:22:08 -0300

    Update InstallScreen.tsx

--- src/components/InstallScreen.tsx
+++ src/components/InstallScreen.tsx
@@ -18,7 +18,7 @@ export default function InstallScreen({ onInstall, isIOS, canInstall }: InstallS
       >
         <div className="mb-6">
           <img
-            src="https://i.ibb.co/JFKTr9sc/logo.png"
+            src="https://i.ibb.co/ZzWybmSF/logo-cecy.png"
             alt="Logo"
             className="w-24 h-24 mx-auto object-contain"
           />
